4fc75b027c3704cb7e7ad511fed1ac4b171c7323
vaibhavs10 <vaibhavs10@gmail.com>
2021-05-04 00:40:25 +0200

    Updated with a test case - v2

--- experiments.py
+++ experiments.py
@@ -5,12 +5,6 @@ from utils import vectorizer
 
 file_path = "utils/data/processed_data/train.csv"
 
-list_of_rows = vectorizer.parse_and_return_rows(file_path)
-vocab, len_vocab = vectorizer.return_len_and_vocabulary(list_of_rows)
-index_word = vectorizer.create_token_index(vocab)
-test = vectorizer.preprocess_sentence("Chad is a boy")
-print(vectorizer.return_count_vector(test, index_word, len_vocab))
-
 def run_cosine_similarity_exp(file_path):
     list_of_rows = vectorizer.parse_and_return_rows(file_path)
     vocab, len_vocab = vectorizer.return_len_and_vocabulary(list_of_rows)
