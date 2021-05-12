3f568c3b276124b019ce3b4b663d1278a1968de3
vaibhavs10 <vaibhavs10@gmail.com>
2021-05-12 14:10:58 +0200

    Updated experiments for entire train set and test on dev set

--- experiments.py
+++ experiments.py
@@ -5,39 +5,39 @@ from utils import vectorizer
 
 # Experiment1: Cosine similarity between averaged obs1, obs2 vectors and hypothesis vectors
 
-file_path = "utils/data/processed_data/train.csv"
+train_file_path = "utils/data/processed_data/train.csv"
+test_file_path = "utils/data/processed_data/dev.csv"
 
-def run_cosine_similarity_exp(file_path):
-    list_of_rows = vectorizer.parse_and_return_rows(file_path)
-    vocab, len_vocab = vectorizer.return_len_and_vocabulary(list_of_rows)
-    index_word = vectorizer.create_token_index(vocab)
+def run_cosine_similarity_exp(train_file_path, test_file_path):
+    train_list_of_rows = vectorizer.parse_and_return_rows(train_file_path)
+    test_list_of_rows = vectorizer.parse_and_return_rows(test_file_path)
+    train_vocab, train_len_vocab = vectorizer.return_len_and_vocabulary(train_list_of_rows)
+    index_word = vectorizer.create_token_index(train_vocab)
     hyp1_sim = []
     hyp2_sim = []
-    for row in list_of_rows:
+    count = 0
+
+    for row in test_list_of_rows:
         obs1 = vectorizer.preprocess_sentence(row[1])
-        print(obs1)
-        obs1_vec = vectorizer.return_count_vector(obs1, index_word, len_vocab)
+        obs1_vec = vectorizer.return_count_vector(obs1, index_word, train_len_vocab)
         obs2 = vectorizer.preprocess_sentence(row[2])
-        print(obs2)
-        obs2_vec = vectorizer.return_count_vector(obs2, index_word, len_vocab)
+        obs2_vec = vectorizer.return_count_vector(obs2, index_word, train_len_vocab)
         hyp1 = vectorizer.preprocess_sentence(row[3])
-        print(hyp1)
-        hyp1_vec = vectorizer.return_count_vector(hyp1, index_word, len_vocab)
+        hyp1_vec = vectorizer.return_count_vector(hyp1, index_word, train_len_vocab)
         hyp2 = vectorizer.preprocess_sentence(row[4])
-        print(hyp2)
-        hyp2_vec = vectorizer.return_count_vector(hyp2, index_word, len_vocab)
+        hyp2_vec = vectorizer.return_count_vector(hyp2, index_word, train_len_vocab)
 
         avg_obs_vec = (obs1_vec + obs2_vec)/2
-        print("-_________-")
-        print(similarity.cosine_similarity(avg_obs_vec, hyp1_vec))
-        print(similarity.cosine_similarity(avg_obs_vec, hyp2_vec))
+
         hyp1_sim.append(similarity.cosine_similarity(avg_obs_vec, hyp1_vec))
         hyp2_sim.append(similarity.cosine_similarity(avg_obs_vec, hyp2_vec))
 
-        with open("hyp1_sim.txt", "w") as fp:
-            json.dump(hyp1_sim, fp)
+        print(count+=1)
 
-        with open("hyp2_sim.txt", "w") as fp:
-            json.dump(hyp2_sim, fp)
+    with open("hyp1_sim.txt", "w") as fp:
+        json.dump(hyp1_sim, fp)
 
-run_cosine_similarity_exp(file_path)
+    with open("hyp2_sim.txt", "w") as fp:
+        json.dump(hyp2_sim, fp)
+
+run_cosine_similarity_exp(train_file_path, test_file_path)
