2299184a355c34ce5bc33e887f259d6c280aecf6
vaibhavs10 <vaibhavs10@gmail.com>
2021-05-12 16:29:59 +0200

    Complete experiment 1

--- experiments.py
+++ experiments.py
@@ -1,12 +1,17 @@
 import json
+import random
 
 from utils import similarity
 from utils import vectorizer
+from utils import evaluation
 
 # Experiment1: Cosine similarity between averaged obs1, obs2 vectors and hypothesis vectors
 
 train_file_path = "utils/data/processed_data/train.csv"
 test_file_path = "utils/data/processed_data/dev.csv"
+
+hyp1_sim_file_path = "hyp1_sim.txt"
+hyp2_sim_file_path = "hyp2_sim.txt"
 
 def run_cosine_similarity_exp(train_file_path, test_file_path):
     train_list_of_rows = vectorizer.parse_and_return_rows(train_file_path)
@@ -42,4 +47,33 @@ def run_cosine_similarity_exp(train_file_path, test_file_path):
     with open("hyp2_sim.txt", "w") as fp:
         json.dump(hyp2_sim, fp)
 
-run_cosine_similarity_exp(train_file_path, test_file_path)
+def calc_test_similarity_accuracy(test_file_path, hyp1_sim_file_path, hyp2_sim_file_path):
+
+    with open(hyp1_sim_file_path, "r") as fp:
+        hyp1_sim = json.load(fp)
+
+    with open(hyp2_sim_file_path, "r") as fp:
+        hyp2_sim = json.load(fp)
+
+    pred_list = []
+
+    for i in range(len(hyp1_sim)):
+        if hyp1_sim[i] > hyp2_sim[i]:
+            pred_list.append("1")
+        elif hyp1_sim[i] < hyp2_sim[i]:
+            pred_list.append("2")
+        elif hyp1_sim[i] == hyp2_sim[i]:
+            rand_choice = random.choice(["1","2"])
+            pred_list.append(rand_choice)
+    
+    test_list_of_rows = vectorizer.parse_and_return_rows(test_file_path)
+    orig_list = []
+    for row in test_list_of_rows:
+        orig_list.append(row[5])
+
+    accuracy = evaluation.calculate_accuracy(pred_list, orig_list)
+    print(len(orig_list))
+    print(len(pred_list))
+    print(accuracy)
+#run_cosine_similarity_exp(train_file_path, test_file_path)
+calc_test_similarity_accuracy(test_file_path, hyp1_sim_file_path, hyp2_sim_file_path)
