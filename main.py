f6c60f897fa2f451723cbaa1e8e4cdef06af0672
Emils K <kadikis.emils@gmail.com>
2021-06-27 17:16:33 +0200

    Expanding experiment definitions, so that trying you don't have to copy all the hyperparameters many times to try varying just one or two hyperparameters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import importlib
 import argparse
 import time
 import os
+from itertools import product
 # setting up command line interface
 parser = argparse.ArgumentParser(description='Various experiments for abductive natural language inferrence.')
 parser.add_argument('--infile', nargs=1,
@@ -50,12 +51,53 @@ def save_results(experiment_definition, labels, accuracy, logs):
         file.writelines(logs)
         file.close()
 
+def product_dict(**kwargs):
+    """
+    Given a dictionary such as {a: [1,2,3], b: [4,5,6]}
+    Iterates through all possbiel combinations: [{a: 1, b: 4}, {a: 1, b: 5}, {a: 1, b: 6}, ....]
+
+    Code taken from: https://stackoverflow.com/questions/5228158/cartesian-product-of-a-dictionary-of-lists
+    """
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in product(*vals):
+        yield dict(zip(keys, instance))
+
+
+def expand_experiments(experiments):
+    """
+    For every experiment that has 'try_combinations': [column_a, column_b, ...] defined, 
+    expands it into multiple separate experiments, trying all possible combinations of the values given in column_a, column_b, ....
+    """
+    expanded_experiments = []
+    for experiment_definition in experiments:
+        if 'try_combinations' in experiment_definition:
+            column_names = experiment_definition['try_combinations']
+            all_possible_values = {column_name: experiment_definition['hyperparameters'][column_name] for column_name in column_names}
+            all_possible_value_combinations = product_dict(**all_possible_values)
+
+            # create a new experiment definition for each combination
+            for combination in all_possible_value_combinations:
+                new_experiment_definition = experiment_definition.copy()
+                new_experiment_definition['hyperparameters'] = experiment_definition['hyperparameters'].copy()
+
+                new_experiment_definition.pop('try_combinations')
+                # change the value of each hyperparameter that are being varied
+                for key, value in combination.items():
+                    new_experiment_definition['hyperparameters'][key] = value
+                expanded_experiments.append(new_experiment_definition)
+        else:
+            expanded_experiments.append(experiment_definition)
+    return expanded_experiments
+
+
 
 if __name__ == "__main__":
     #args.infile = [open('wmd_similarity.json', 'r')] # uncomment this to run an experiment without having to pass it in the command line
     if args.infile is not None and args.infile[0] is not None:
         # load the passed json file that contains details about the experiment to run
         all_experiments = json.load(args.infile[0])['experiments']
+        all_experiments = expand_experiments(all_experiments)
         for experiment_definition in all_experiments:
             id = experiment_definition['experiment_id']
             try:
