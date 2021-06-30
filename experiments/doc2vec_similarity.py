419ad4608e0189c2dae363974f1bcc605ab83f95
Emils K <kadikis.emils@gmail.com>
2021-06-30 15:09:26 +0200

    doc2vec experiment refactor

--- experiments/doc2vec_similarity.py
+++ experiments/doc2vec_similarity.py
@@ -1,12 +1,11 @@
-from scipy import spatial
-import importlib
-
+import scipy.spatial as spatial
 import models.evaluation as evaluation
 import feature_engineering.document_embeddings as document_embeddings
+from experiments.experiment_utilities import get_param, load_data, printt, set_trace
 
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.WARNING)
-    
+
 def _predict_labels(model, data):
     labels = []
     for instance in data:
@@ -20,64 +19,64 @@ def _predict_labels(model, data):
             labels.append('1')
         else:
             labels.append('2')
-    return labels
-
-def _load_data(dev_data_loader_name, embedding_data_loader_name, trace=False):
-    embedding_training_corpus = None
-
-    dev_data_loader = importlib.import_module("experiments." + dev_data_loader_name)
-    dev_documents, _ = dev_data_loader.load_data()
-    if embedding_data_loader_name:
-        embedding_data_loader = importlib.import_module("experiments." + embedding_data_loader_name)
-        _, embedding_training_corpus = embedding_data_loader.load_data()
-    return dev_documents, embedding_training_corpus
-       
-def _get_param(dict, key, fallback_value):
-    if key in dict:
-        return dict[key]
-    else:
-        return fallback_value
+    return labels    
 
 def _run_doc2vec_experiment(    
     use_pre_trained_embeddings,
     embedding_type,
     dev_data_loader_name,
     embedding_training_data_loader_name,
-    trace=True):
+    vector_size,
+    min_count):
 
     logs=[]
 
-    if trace:
-        print("Loading data...")
-    dev_documents, embedding_training_corpus = _load_data(dev_data_loader_name, embedding_training_data_loader_name, trace)
+    printt("Loading data...")
+    dev_documents, embedding_training_corpus = load_data(dev_data_loader_name, embedding_training_data_loader_name)
     if use_pre_trained_embeddings:
-        if trace:
-            print("Using pretrained embedding model %s ..." % embedding_type)
+        printt("Using pretrained embedding model %s ..." % embedding_type)
         embedding_model = document_embeddings.get_pretrained_embeddings(embedding_type)
     else:
-        if trace:
-            print("Training %s embedding model..." % embedding_type)
-        embedding_model = document_embeddings.train_embedding_model(embedding_training_corpus, embedding_type)
+        printt("Training %s embedding model..." % embedding_type)
+        embedding_model = document_embeddings.train_embedding_model(embedding_training_corpus, embedding_type, vector_size, min_count)
         
-    if trace:
-        print("Predicting labels on devset...")
+    printt("Predicting labels on devset...")
     predicted_labels = _predict_labels(embedding_model, dev_documents)
     real_labels = [x[4] for x in dev_documents]
     acc = evaluation.calculate_accuracy(predicted_labels, real_labels)
 
-    if trace:
-        print("Accuracy: " + str(acc))
+    printt("Accuracy: " + str(acc))
     logs.append("Accuracy: " + str(acc))
 
     return predicted_labels, logs
 
 
 def run(ex):
+    """
+    Doc2vec experiment with cosine similarity.
+    Takes in a dictionary with all the hyperparameters, for example:
+        {
+            "trace": true,     
+
+            "hyperparameters" : {
+                "dev_data_loader_name": "data_loaders.dev_data_loader",
+                "pre_trained_embeddings":false,
+                "embedding_type":"word2vec",
+                "embedding_training_data_loader_name": "data_loaders.ROC_data_loader",
+                "vector_size":100,
+                "min_count":10
+            }
+        }
+    
+    Returns: predicted labels, logs
+    """  
     hp = ex["hyperparameters"]
+    set_trace(get_param(ex, "trace", True))
     return _run_doc2vec_experiment(
-        use_pre_trained_embeddings=_get_param(hp, "pre_trained_embeddings", False),
-        embedding_type=_get_param(hp, "embedding_type", "doc2vec"),
-        dev_data_loader_name=_get_param(hp, "dev_data_loader_name", "word2vec"),
-        embedding_training_data_loader_name=_get_param(hp, "embedding_training_data_loader_name", "word2vec"),
-        trace=_get_param(ex, "trace", True),
+        use_pre_trained_embeddings=get_param(hp, "pre_trained_embeddings", False),
+        embedding_type=get_param(hp, "embedding_type", "doc2vec"),
+        dev_data_loader_name=get_param(hp, "dev_data_loader_name", "word2vec"),
+        embedding_training_data_loader_name=get_param(hp, "embedding_training_data_loader_name", "word2vec"),
+        vector_size=get_param(hp, "vector_size", 100),
+        min_count=get_param(hp, "min_count", 10),
         )
